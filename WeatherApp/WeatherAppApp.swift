55f2b57240e397c127392ee8bb48708ea4ad7b2f
Kevinwtierney <kevinwtierney@gmail.com>
2021-11-19 09:19:33 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kevin Tierney on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
